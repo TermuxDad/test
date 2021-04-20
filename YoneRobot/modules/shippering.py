c1ac79e3a4ab340863b31a116ea4e75301e85a85
TermuxDad <76565235+TermuxDad@users.noreply.github.com>
2021-04-20 14:03:45 +0530

    Update shippering.py

--- YoneRobot/modules/shippering.py
+++ YoneRobot/modules/shippering.py
@@ -76,9 +76,10 @@ __New couple of the day may be chosen at 12AM {tomorrow}__"""
     #     await message.reply_text(e)
 
 
+__mod_name__ = "Couples"
 
 __help__ = """
  ❍ /couples - To Choose Couple Of The Day
+"""
 
- """
-__mod_name__ = "Couples"
+
